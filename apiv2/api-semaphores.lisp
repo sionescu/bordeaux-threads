dae48d5241a95337aab6f2f1276cf67c1bfb5a93
Stelian Ionescu <sionescu@cddr.org>
2022-01-02 23:01:59 -0500

    Extract semaphore implementation to separate functions
    Also clarify the docstring of WAIT-ON-SEMAPHORE.

--- apiv2/api-semaphores.lisp
+++ apiv2/api-semaphores.lisp
@@ -18,52 +18,54 @@
   (check-type name (or null string))
   (%make-semaphore name count))
 
-(defun signal-semaphore (semaphore &key (count 1))
-    "Increment SEMAPHORE by COUNT. If there are threads waiting on this
-semaphore, then COUNT of them are woken up."
-  #+(or allegro ccl ecl lispworks mezzano sbcl)
-  (%signal-semaphore semaphore count)
-  #-(or allegro ccl ecl lispworks mezzano sbcl)
+#-(or allegro ccl ecl lispworks mezzano sbcl)
+(defun %signal-semaphore (semaphore count)
   (with-lock-held ((%semaphore-lock semaphore))
     (incf (%semaphore-counter semaphore) count)
     (dotimes (v count)
-      (%condition-notify (%semaphore-condition-variable semaphore))))
+      (%condition-notify (%semaphore-condition-variable semaphore)))))
+
+(defun signal-semaphore (semaphore &key (count 1))
+  "Increment SEMAPHORE by COUNT. If there are threads waiting on this
+semaphore, then COUNT of them are woken up."
+  (%signal-semaphore semaphore count)
   t)
 
-(defun wait-on-semaphore (semaphore &key timeout)
-  "Decrement the count of SEMAPHORE by 1 if the count would not be negative.
-
-Else blocks until the semaphore can be decremented. Returns generalized boolean
-T on success.
-
-If TIMEOUT is given, it is the maximum number of seconds to wait. If the count
-cannot be decremented in that time, returns NIL without decrementing the count."
-  #+(or allegro ccl ecl lispworks mezzano sbcl)
-  (%wait-on-semaphore semaphore timeout)
-  #-(or allegro ccl ecl lispworks mezzano sbcl)
+#-(or allegro ccl ecl lispworks mezzano sbcl)
+(defun %wait-on-semaphore (semaphore timeout)
   (with-lock-held ((%semaphore-lock semaphore))
-    (if (>= (%semaphore-counter semaphore) 1)
+    (if (plusp (%semaphore-counter semaphore))
         (decf (%semaphore-counter semaphore))
         (let ((deadline (when timeout
                           (+ (get-internal-real-time)
                              (* timeout internal-time-units-per-second)))))
           ;; we need this loop because of a spurious wakeup possibility
-          (loop until (>= (%semaphore-counter semaphore) 1)
-             do (cond
-                  ((null (%condition-wait
-                          (%semaphore-condition-variable semaphore)
-                          (lock-native-lock (%semaphore-lock semaphore))
-                          timeout))
-                   (return-from wait-on-semaphore))
-                  ;; unfortunately cv-wait may return T on timeout too
-                  ((and deadline (>= (get-internal-real-time) deadline))
-                   (return-from wait-on-semaphore))
-                  (timeout
-                   (setf timeout (/ (- deadline (get-internal-real-time))
-                                    internal-time-units-per-second)))))
+          (loop until (plusp (%semaphore-counter semaphore))
+                do (cond
+                     ((null (%condition-wait
+                             (%semaphore-condition-variable semaphore)
+                             (lock-native-lock (%semaphore-lock semaphore))
+                             timeout))
+                      (return-from wait-on-semaphore))
+                     ;; unfortunately cv-wait may return T on timeout too
+                     ((and deadline (>= (get-internal-real-time) deadline))
+                      (return-from wait-on-semaphore))
+                     (timeout
+                      (setf timeout (/ (- deadline (get-internal-real-time))
+                                       internal-time-units-per-second)))))
           (decf (%semaphore-counter semaphore))))
     ;; Semaphore acquired.
     t))
+
+(defun wait-on-semaphore (semaphore &key timeout)
+  "Decrement the count of SEMAPHORE by 1 if the count is larger than zero.
+
+If count is zero, blocks until the semaphore can be decremented.
+Returns generalized boolean T on success.
+
+If TIMEOUT is given, it is the maximum number of seconds to wait. If the count
+cannot be decremented in that time, returns NIL without decrementing the count."
+  (%wait-on-semaphore semaphore timeout))
 
 (defun semaphorep (object)
   "Returns T if OBJECT is a semaphore, otherwise NIL."
